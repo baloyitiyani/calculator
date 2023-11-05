71bc12528c799aeb61681fac9759af6a65f298a1
Baloyi Tiyani <qxz4inc@africa.bmw.corp>
2023-11-05 08:01:50 +0200

    modified a spec file indentation (:

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -3,22 +3,18 @@ describe('calculator test', function(){
         const calculator = new Calculator();
         calculator.add(5);
         calculator.add(4)
-
         expect(calculator.total).toBe(9);
     });
     it("should subract two numbers", function(){
         const calculator = new Calculator();
         calculator.total = 50;
-
         calculator.subtract(15);
-
         expect(calculator.total).toBe(35);
     });
 
     it('should multiply two numbers', function(){
         const calculator =  new Calculator();
         calculator.total = 5;
-
         calculator.multiply(6);
         expect(calculator.total).toBe(30);
 
